a8548c20cf241d310e36d257b2f3791ba00cc385
Prajwal Singh <74970268+praje-codes@users.noreply.github.com>
2022-05-19 15:01:27 +0530

    Update run.js

--- scripts/run.js
+++ scripts/run.js
@@ -4,14 +4,7 @@ const main = async () => {
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
   
-    // Call the function.
     let txn = await nftContract.makeAnEpicNFT()
-    // Wait for it to be mined.
-    await txn.wait()
-  
-    // Mint another NFT for fun.
-    txn = await nftContract.makeAnEpicNFT()
-    // Wait for it to be mined.
     await txn.wait()
   
   };
